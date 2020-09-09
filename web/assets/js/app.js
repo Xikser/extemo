c3516e5affc4c5eb04b0cccd9efb85b229ac35e7
Xikser <Xikser@wp.pl>
2020-09-09 21:37:09 +0200

    Create smooth scrolling script

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,42 +1,28 @@
+const content = document.querySelector('.content');
 const header = document.querySelector('.header');
 const about = document.querySelector('.about');
 const services = document.querySelector('.services');
+const projects = document.querySelector('.project');
 const isVisibleClass = 'is--visible';
+
 
 const buttonMap = new Map();
 
 document.querySelectorAll('.home-link').forEach(e => buttonMap.set(e, header))
 document.querySelectorAll('.about-link').forEach(e => buttonMap.set(e, about));
 document.querySelectorAll('.services-link').forEach(e => buttonMap.set(e, services));
+document.querySelectorAll('.projects-link').forEach(e => buttonMap.set(e, projects));
 
-for(const button of buttonMap.keys()) {
-    button.addEventListener('click', setSectionVisible)
+for (const button of buttonMap.keys()) {
+    button.addEventListener('click', setSection)
 }
 
-function setSectionVisible() { 
+function setSection() { 
     const section = buttonMap.get(this);
-    sectionToHide();
-    sectionToShow(section);
-}
+    const offsetTop = section.offsetTop;
 
-function sectionToHide() {
-    const sectionToHideArray = document.querySelectorAll('.is--visible');
     
-    for (let i = 0; i < sectionToHideArray.length; i++) {
-        if (sectionToHideArray[i].classList.contains(isVisibleClass)) {
-            sectionToHideArray[i].classList.remove(isVisibleClass);
-            sectionToHideArray[i].style.setProperty('display', 'none');
-
-            const section = sectionToHideArray[i];
-            transition3D(section);
-        }
-    }
+    content.style.setProperty('transition', 'all 700ms ease');
+    content.style.setProperty('transform', 'translate3d(0,' + -offsetTop + 'px' + ', 0)');
 }
 
-function sectionToShow(section) {
-    section.classList.add(isVisibleClass);
-}
-
-function transition3D(hiddenSection) {
-    hiddenSection.style.setProperty('transform3d', '0, 200%, 0')
-}
---
+++ web/assets/js/windowScroll.js
@@ -0,0 +1,46 @@
+// function normalizeWheelSpeed(event) {
+//     var normalized;
+    
+//     if (event.wheelDelta) {
+//         normalized = (event.wheelDelta % 120 - 0) == -0 ? event.wheelDelta / 120 : event.wheelDelta / 12;
+//     } else {
+//         var rawAmount = event.deltaY ? event.deltaY : event.detail;
+//         normalized = -(rawAmount % 3 ? rawAmount * 10 : rawAmount / 3);
+//     }
+
+//     test(normalized, '');
+//     return normalized;
+// } 
+    
+
+// // window.addEventListener('mousewheel', normalizeWheelSpeed);
+// window.addEventListener('DOMMouseScroll', normalizeWheelSpeed);
+// // window.addEventListener('wheel', normalizeWheelSpeed);
+
+
+// const array = new Array();
+// array.push(header, about, services, projects);
+
+
+// function test(norm, offsetTop) {
+//     console.log(offsetTop);
+//     if (norm > 0) {
+//         content.style.setProperty('transition', 'all 700ms ease');
+//         content.style.setProperty('transform', 'translate3d(0,' + offsetTop + 'px' + ', 0)');
+//     }
+
+//     if (norm < 0) {
+//         content.style.setProperty('transition', 'all 700ms ease');
+//         content.style.setProperty('transform', 'translate3d(0,' + -offsetTop + 'px' + ', 0)');
+//     } 
+// }
+
+// if (e.deltaY < 0) {
+//     content.style.setProperty('transition', 'all 700ms ease');
+//     content.style.setProperty('transform', 'translate3d(0,' + offsetTop + 'px' + ', 0)');
+// }
+
+// if (e.deltaY > 0) {
+//     content.style.setProperty('transition', 'all 700ms ease');
+//     content.style.setProperty('transform', 'translate3d(0,' + -offsetTop + 'px' + ', 0)');
+// }
