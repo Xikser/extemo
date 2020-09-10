fd3041caa8850265d1868b13506ee3f73a15f02c
Xikser <Xikser@wp.pl>
2020-09-10 14:07:10 +0200

    Clean

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,26 +2,25 @@ const content = document.querySelector('.content');
 const header = document.querySelector('.header');
 const about = document.querySelector('.about');
 const services = document.querySelector('.services');
-const projects = document.querySelector('.project');
+const projects = document.querySelector('.projects');
 const isVisibleClass = 'is--visible';
 
 
-const buttonMap = new Map();
+const linkMap = new Map();
 
-document.querySelectorAll('.home-link').forEach(e => buttonMap.set(e, header))
-document.querySelectorAll('.about-link').forEach(e => buttonMap.set(e, about));
-document.querySelectorAll('.services-link').forEach(e => buttonMap.set(e, services));
-document.querySelectorAll('.projects-link').forEach(e => buttonMap.set(e, projects));
+document.querySelectorAll('.home-link').forEach(e => linkMap.set(e, header))
+document.querySelectorAll('.about-link').forEach(e => linkMap.set(e, about));
+document.querySelectorAll('.services-link').forEach(e => linkMap.set(e, services));
+document.querySelectorAll('.projects-link').forEach(e => linkMap.set(e, projects));
 
-for (const button of buttonMap.keys()) {
-    button.addEventListener('click', setSection)
+for (const link of linkMap.keys()) {
+    link.addEventListener('click', setSection)
 }
 
 function setSection() { 
-    const section = buttonMap.get(this);
+    const section = linkMap.get(this);
     const offsetTop = section.offsetTop;
 
-    
     content.style.setProperty('transition', 'all 700ms ease');
     content.style.setProperty('transform', 'translate3d(0,' + -offsetTop + 'px' + ', 0)');
 }
